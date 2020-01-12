37e0eb2c6fa2a84dd59ba7748fdd79ada58701c9
Kate <kate.kalinchenko@gmail.com>
2020-01-13 00:45:14 +0200

    hw 26.12

---
+++ hw_2612/WebContent/index.jsp
@@ -0,0 +1,22 @@
+<html>
+<body>
+	<h2>Hello World!</h2>
+	<a href="init">Init database</a>
+
+	<form method="get" action="SearchServlet">
+		<table>
+			<tr>
+				<td>first name</td>
+				<td><input type="text" name="first"></td>
+			</tr>
+			<tr>
+				<td>last name</td>
+				<td><input type="text" name="last"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="search"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ hw_2612/src/org/itstep/SearchServlet.java
@@ -0,0 +1,79 @@
+package org.itstep;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String first = request.getParameter("first");
+		String last = request.getParameter("last");
+
+		String SELECT = "Select count(*) from people.person where first_name = '" + first + "' AND last_name='" + last
+				+ "';";
+
+		int count = 0;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try (Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://localhost/people?serverTimezone=Europe/Kiev&amp;characterEncoding=utf8", "root", "")) {
+			PrintWriter writer = response.getWriter();
+			Statement statement = conn.createStatement();
+			long startTime = System.currentTimeMillis();
+			ResultSet resultSet = statement.executeQuery(SELECT);
+
+			long time = System.currentTimeMillis() - startTime;
+			while (resultSet.next()) {
+				count = resultSet.getInt(1);
+			}
+			writer.println("There are " + count + " people with searched name\n");
+			writer.println("Execution time: " + time + " ms");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
